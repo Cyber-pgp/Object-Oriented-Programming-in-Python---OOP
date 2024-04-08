e29c2da6d1b0052b654f9811767e316fe7ac7316
Dean Svahnalam <56052788+Cyber-pgp@users.noreply.github.com>
2024-04-09 01:09:52 +0200

    Create lists_and_loops.py

---
+++ lists_and_loops.py
@@ -0,0 +1,31 @@
+# A list is aPython container that can store anything we want.
+
+# we can have:
+# Empty list - empty = []
+# list of strings - string = ['lol', 'wtf', 'haha']
+# list of numbers - nums = [5, 6, 9, 1234]
+# list of mixed items - mixed = [2, 'fuck', 3.09]
+# list of lists - list = [['a', 'b', 'c'], [1,2,3]]
+
+# creating a list of internet slang acronyms
+
+acronyms = ['lol', 'idk', 'smh', 'tbh']
+# index would be 0,1,2,3
+
+print(acronyms([0]) # we type the name of the list followed by square and the index 0.
+print(acronyms([3])
+print(acronyms([-1])
+
+
+acronynms = []
+
+acronyms.append('lol') # with append we can add things to the list
+acronyms.append('idk')
+acronyms.append([5])
+
+print(acronyms)
+
+
+      
+      
+
